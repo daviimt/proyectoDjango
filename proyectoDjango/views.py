6f41f2801bfb92054a6b325500bc37389aff9a58
rxjasss <ruben_07@hotmail.es>
2022-12-19 17:51:21 +0100

    modularizar html

--- proyectoDjango/views.py
+++ proyectoDjango/views.py
@@ -1,43 +1,45 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
 
 def HolaMundo(request): #primera vista
 
-    documento="""<html>
-    <body>
-    <h1>
-    Hola mundo
-    </html>
-    </body>
-    </h1>"""
+    doc_externo=open("C:/Users/ruben/OneDrive/Escritorio/2DAM/PSP/UD4/proyectoDjango/proyectoDjango/plantillas/holamundo.html")
 
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+
+    ctx=Context()
+    documento=plt.render(ctx)
+    
     return HttpResponse(documento)
 
 def fecha(request):
     fecha_actual=datetime.datetime.now()
 
-    documento="""<html>
-    <body>
-    <h2>
-    Fecha y hora actuales %s
-    </h2>
-    </body>
-    </html>""" %fecha_actual
+    doc_externo=open("C:/Users/ruben/OneDrive/Escritorio/2DAM/PSP/UD4/proyectoDjango/proyectoDjango/plantillas/fecha.html")
+
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+
+    ctx=Context({"ahora":fecha_actual})
+    documento=plt.render(ctx)
 
     return HttpResponse(documento)
 
 def calculoEdad(request, edad, year):    
     edadFutura=edad+year
     yearFuturo= 2023+year
-    documento="""<html>
-    <head>
-    <title>
-       Calculo edad
-    </title>
-    </head>
-    <body>
-        <h1>En el año %s tendrás %s años</h1>
-    </body>
-    </html>""" %(yearFuturo,edadFutura)
-    
+
+    doc_externo=open("C:/Users/ruben/OneDrive/Escritorio/2DAM/PSP/UD4/proyectoDjango/proyectoDjango/plantillas/calcularEdad.html")
+
+    plt=Template(doc_externo.read())
+    doc_externo.close()
+
+    ctx=Context({"proximaEdad":edadFutura, "proximoYear":yearFuturo})
+
+    documento=plt.render(ctx)
+
     return HttpResponse(documento)
+    
+    
