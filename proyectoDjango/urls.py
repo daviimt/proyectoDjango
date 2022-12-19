09134841bfe531c1e95bcc2ee1c844d962996e1f
rxjasss <ruben_07@hotmail.es>
2022-12-19 13:19:54 +0100

    fecha y hora

--- proyectoDjango/urls.py
+++ proyectoDjango/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from proyectoDjango.views import HolaMundo
+from proyectoDjango.views import HolaMundo, fecha
 
 urlpatterns = [
     path('admin/', admin.site.urls),
       path('holamundo/', HolaMundo),
+      path('fecha/', fecha),
 ]
--- proyectoDjango/views.py
+++ proyectoDjango/views.py
@@ -1,4 +1,27 @@
 from django.http import HttpResponse
+import datetime
 
 def HolaMundo(request): #primera vista
-    return HttpResponse("Hola Mundo")
+
+    documento="""<html>
+    <body>
+    <h1>
+    Hola mundo
+    </html>
+    </body>
+    </h1>"""
+
+    return HttpResponse(documento)
+
+def fecha(request):
+    fecha_actual=datetime.datetime.now()
+
+    documento="""<html>
+    <body>
+    <h2>
+    Fecha y hora actuales %s
+    </h2>
+    </body>
+    </html>""" %fecha_actual
+
+    return HttpResponse(documento)
